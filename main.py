84f9a859e65cb3de6202011678c9de53e190e15a
Alex4-7 <100441080+Alex4-7@users.noreply.github.com>
2022-06-13 18:49:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from aiogram import Bot, Dispatcher, executor, types
 
 # Объект бота
@@ -11,55 +12,15 @@ logging.basicConfig(level=logging.INFO)
 
 @dp.message_handler(commands="start")
 async def start_answer(message: types.Message):
-    await message.answer("Приветствую.\nЭтот бот умеет редактировать видео, для работы с ним пришлите одно.")
+    await message.answer("Приветствую.\nЭтот бот умеет искать песню по ключевому слову, для работы с ним пришлите одно.")
 
-@dp.message_handler(content_types=["video"])
-async def video_answer(message: types.Message):
-	keyboard = types.InlineKeyboardMarkup()
-	keyboard.add(types.InlineKeyboardButton(text="Добавить звук", callback_data="b1"))
-	keyboard.add(types.InlineKeyboardButton(text="Убрать звук", callback_data="b2"))
-	await message.answer("Ты прислал видео!\nЧто пожелаешь с ним сделать?",reply_markup=keyboard)
-	await message.video.download() # вот строка, которая должна скачивать видос, каким образом это происходит я хз
-
-
-@dp.callback_query_handler(text="b1") # Добавить звук
-async def b1(call: types.CallbackQuery):
-	keyboard = types.InlineKeyboardMarkup()
-	keyboard.add(types.InlineKeyboardButton(text="Я пришлю файл", callback_data="b11"))
-	keyboard.add(types.InlineKeyboardButton(text="Я пришлю голосовое сообщение", callback_data="b12"))
-	#keyboard.add(types.InlineKeyboardButton(text="Я хочу воспользоваться встроенным поиском", callback_data="b13"))
-	await call.message.answer("Каким способом вы хотите прислать аудио?",reply_markup=keyboard)
-	
-
-@dp.callback_query_handler(text="b2") # Убрать звук
-async def b2(message: types.Message):
-	# убрать звук()
-    #await bot.send_video(/vi\)
-    await message.answer("звук убран")
-
-
-@dp.callback_query_handler(text="b11")
-async def b11(call: types.CallbackQuery):
-    await call.message.answer('пришлите файл')
-
-
-@dp.callback_query_handler(text="b12")
-async def b12(call: types.CallbackQuery):
-	await call.message.answer('пришлите голосовое сообщение')
-
-
-@dp.message_handler(content_types=["audio"])
-async def audio_answer(message):
-	await message.audio.download()
-	#await bot.send_message(message.chat.id, "Аудио.")
-	#await bot.send_video(/vi\)
-
-@dp.message_handler(content_types=["voice"])
-async def voice_answer(message):
-	await message.voice.download()
-	#await bot.send_message(message.chat.id, "voice.")
-	#await bot.send_video(/vi\)
-
+@dp.message_handler(content_types=["text"])
+async def process_voice_command(message: types.Message):
+	listmusic = os.listdir('music')
+	for i in listmusic:
+		audio = open(r'music/' + i, 'rb')
+		await bot.send_audio(message.chat.id, audio)
+		audio.close()
 
 
 
